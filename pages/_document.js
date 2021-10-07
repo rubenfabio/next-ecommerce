2d79c530e27cf8db2dc19e95e28511bf991a3b94
Ruben Fabio Gutierrez Gongora <ruben.fabio2016@gmail.com>
2021-10-07 07:53:02 -0300

    update

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import { ServerStyleSheets } from '@material-ui/core/styles';
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
